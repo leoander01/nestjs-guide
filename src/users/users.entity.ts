21c63a8ac802bee81547c553322d5f3d018897a5
Leoander Neves <leoanderneves8@outlook.com>
2024-03-31 12:09:53 -0300

    add service methods in user service

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,13 +1,28 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
   @Column()
   email: string;
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
+  
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id);
+  }
+  
+  @AfterInsert()
+  logDelete() {
+    console.log('Deleted user with id', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,29 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email }});
+  }
+  
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+  
+  async remove(id: number) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
